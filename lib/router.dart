8bf5785da64d74d738e3614de95ee9a775f8ec4f
Emre Demirel <fed3224@gmail.com>
2024-11-08 17:56:49 +0300

    Gloabal variables has been added. Router file has been created. Auth Screen file has been created

---
+++ lib/router.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_amazon_clone/features/screens/auth_screen.dart';
+
+Route<dynamic> generateRoute(RouteSettings routeSettings) {
+  switch (routeSettings.name) {
+    case AuthScreen.routeName:
+      return MaterialPageRoute(
+        settings: routeSettings,
+        builder: (_) => const AuthScreen(),
+      );
+    //We can do an Error Page at the end for default
+    default:
+      return MaterialPageRoute(
+          settings: routeSettings,
+          builder: (_) => const Scaffold(
+                body: Center(
+                  child: Text("Page does not exist"),
+                ),
+              ));
+  }
+}
