b6255571491737f7a36e88a66f687fb62fbfc41b
Emre Demirel <fed3224@gmail.com>
2024-11-11 14:29:56 +0300

    Auth screen has been designed. Custom button and custom textfiled common widgets that are going to be used for the entire app, are designed

---
+++ server/index.js
@@ -0,0 +1,33 @@
+//Import packages
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+//Import from other files 
+
+const authRouter = require('./routes/auth');
+const mongoDbUri = require('./constants/database_variables')
+
+
+//INIT
+
+const PORT = 3000;
+const app = express();
+const DB = mongoDbUri;
+
+//middleware 
+
+app.use(authRouter);
+
+//connections
+
+mongoose
+.connect(DB)
+.then(() => {
+    console.log("connection to database is successfull")
+})
+.catch((e) => {
+    console.log(e)
+});
+
+app.listen(PORT, '0.0.0.0', () => {console.log(`Connected at ${PORT}`)})
---
+++ server/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name:{
+        required: true,
+        type: String,
+        trim: true
+    },
+    email:{
+        required: true,
+        type: String,
+        trim: true,
+        validate: {
+            validator : (val) => {
+                const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+                return val.match(re);
+            }
+        }
+    }
+})
