d1d7ca95bb9ac2ebb00f6cc2db16592f7b681d6b
emredmrell <fed3224@gmail.com>
2024-12-14 14:27:36 +0300

    user.js file added

---
+++ server/routes/user.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const userRouter = express.Router();
+const auth_middleware = require('../middlewares/auth_middleware');
+const {Product} = require('../models/product.js');
+const User = require('../models/user.js');
+
+//add product
+userRouter.post('/api/add-to-cart', auth_middleware, async (req, res) => {
+    try {
+        const { id } = req.body;
+        const product = await Product.findById(id);
+        let user = await User.findById(req.user);
+
+        if (user.cart.length == 0){
+            user.cart.push({product, quantity : 1});
+        }else{
+            let isProductFound = false;
+            for(let i = 0; i < user.cart.length ; i++){
+                if(user.cart[i].product._id.equals(product._id)){
+                    isProductFound = true;
+                }
+            }
+
+            if(isProductFound){
+                let productToIncrease = user.cart.find(
+                    (e) => 
+                        e.product._id.equals(product._id)
+                );
+                productToIncrease.quantity +=1;
+            }else{
+                user.cart.push({product, quantity : 1})
+            }
+        }
+        user = await user.save();
+        res.json(user);
+    } catch (e) {
+        res.status(500).json({error : e.message});
+    }
+});
+
+module.exports = userRouter
